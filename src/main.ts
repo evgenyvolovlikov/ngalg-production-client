cf778fb1cd1443a4905a4b66ac1ecf944b2f9f72
Евгений Воловликов <evgenyvolovlikov@gmail.com>
2026-09-18 17:58:41 +0300

    feat: add base config for app

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, ChangeDetectionStrategy, Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+    providers: [],
+};
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: `<div>Hello World</div>`,
+    imports: [],
+})
+export class AppComponent {}
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
